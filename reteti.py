1c927e3b30c1d192e9541a73d1797bb3fd60bb23
Dimitar D. Mitov <ddmitov@gmail.com>
2024-09-11 10:32:46 +0300

    search and logging updates

--- reteti.py
+++ reteti.py
@@ -10,7 +10,6 @@ from dotenv import load_dotenv
 import duckdb
 import pandas          as pd
 import pyarrow         as pa
-import pyarrow.dataset as ds
 import pyarrow.fs      as fs
 import pyarrow.parquet as pq
 from tokenizers import Tokenizer
@@ -158,10 +157,9 @@ def reteti_batch_indexer(
                 '''
             ).fetch_arrow_table().to_pandas()['tokens'].iloc[0]
 
-            date_time_now = datetime.datetime.now()
-            date_time_string = (
-                date_time_now.strftime('%Y-%m-%d--%H-%M-%S').strip()
-            )
+            date_time = datetime.datetime.now()
+            date_time_string = \
+                date_time.strftime('%Y-%m-%d--%H-%M-%S').strip()
 
             pq.write_to_dataset(
                 tokens_arrow_table,
@@ -223,10 +221,13 @@ def reteti_searcher(token_list: list) -> tuple[dict, dict]:
     # Step 1 - read token data:
     step_01_start_time = time.time()
 
+    # If any token is repeated in the search request,
+    # the respective token data is being read only once:
+    token_set = set(token_list)
     token_arrow_tables_list = []
 
-    for token in token_list:
-        # If a token does not exist in the dataset,
+    for token in token_set:
+        # If a token does not exist in the tokens dataset,
         # this must not fail the extraction of the existing token data:
         try:
             token_arrow_table = pq.ParquetDataset(
@@ -240,12 +241,10 @@ def reteti_searcher(token_list: list) -> tuple[dict, dict]:
 
     tokens_arrow_table = pa.concat_tables(token_arrow_tables_list)
 
-    step_01_time = time.time() - step_01_start_time
+    step_01_time = round((time.time() - step_01_start_time), 3)
 
-    # Step 2 - get the top N matching text IDs:
+    # Step 2 - get the top N text IDs:
     step_02_start_time = time.time()
-
-    unique_tokens_number = len(set(token_list))
 
     top_results_arrow_table = duckdb.sql(
         f'''
@@ -256,7 +255,7 @@ def reteti_searcher(token_list: list) -> tuple[dict, dict]:
             FROM tokens_arrow_table tat
             GROUP BY text_id
             HAVING
-                unique_matching_tokens = {unique_tokens_number}
+                unique_matching_tokens = {len(token_set)}
                 AND total_matching_tokens >= {len(token_list)}
             ORDER BY total_matching_tokens DESC
             LIMIT 10
@@ -270,9 +269,9 @@ def reteti_searcher(token_list: list) -> tuple[dict, dict]:
         '''
     ).fetch_arrow_table().to_pandas()['text_id'].to_list()
 
-    step_02_time = time.time() - step_02_start_time
+    step_02_time = round((time.time() - step_02_start_time), 3)
 
-    # Step 3 - get the top N matching texts:
+    # Step 3 - get the top N texts:
     step_03_start_time = time.time()
 
     text_parquet_paths = []
@@ -286,16 +285,22 @@ def reteti_searcher(token_list: list) -> tuple[dict, dict]:
         filesystem=parquet_dataset_filesystem
     ).read()
 
-    step_03_time = time.time() - step_03_start_time
+    step_03_time = round((time.time() - step_03_start_time), 3)
 
-    # Step 4 - get the final search results:
+    # Step 4 - get the final results:
     step_04_start_time = time.time()
+
+    step_01_label = 'Step 1 - read token data ........ runtime in seconds:'
+    step_02_label = 'Step 2 - get the top N text IDs . runtime in seconds:'
+    step_03_label = 'Step 3 - get the top N texts .... runtime in seconds:'
+    step_04_label = 'Step 4 - get the final results .. runtime in seconds:'
+    total_label   = 'Total ........................... runtime in seconds:'
 
     try:
         search_result_dataframe = duckdb.query(
             '''
                 SELECT
-                    CAST(tr.total_matching_tokens AS INT) AS total_matching_tokens,
+                    CAST(tr.total_matching_tokens AS INT) AS matching_tokens,
                     t.text_id,
                     t.* EXCLUDE (text_id, text),
                     t.text
@@ -307,33 +312,39 @@ def reteti_searcher(token_list: list) -> tuple[dict, dict]:
             '''
         ).fetch_arrow_table().to_pandas()
     except Exception:
-        step_04_time = time.time() - step_04_start_time
+        step_04_time = round((time.time() - step_04_start_time), 3)
 
-        total_time = step_01_time + step_02_time + step_03_time + step_04_time
+        total_time = round(
+            (step_01_time + step_02_time + step_03_time + step_04_time),
+            3
+        )
 
         search_info = {}
-        search_info['Step 1 runtime in seconds:'] = step_01_time
-        search_info['Step 2 runtime in seconds:'] = step_02_time
-        search_info['Step 3 runtime in seconds:'] = step_03_time
-        search_info['Step 4 runtime in seconds:'] = step_04_time
-        search_info['Total runtime in seconds: '] = total_time
+        search_info[step_01_label] = step_01_time
+        search_info[step_02_label] = step_02_time
+        search_info[step_03_label] = step_03_time
+        search_info[step_04_label] = step_04_time
+        search_info[total_label]   = total_time
 
         search_result = {}
-        search_result['Search Result:'] = 'No matching texts were found.'
+        search_result['Message:'] = 'No matching texts were found.'
 
         return search_info, search_result
 
     search_result = search_result_dataframe.to_dict('records')
 
-    step_04_time = time.time() - step_04_start_time
+    step_04_time = round((time.time() - step_04_start_time), 3)
 
-    total_time = step_01_time + step_02_time + step_03_time + step_04_time
+    total_time = round(
+        (step_01_time + step_02_time + step_03_time + step_04_time),
+        3
+    )
 
     search_info = {}
-    search_info['Step 1 runtime in seconds:'] = step_01_time
-    search_info['Step 2 runtime in seconds:'] = step_02_time
-    search_info['Step 3 runtime in seconds:'] = step_03_time
-    search_info['Step 4 runtime in seconds:'] = step_04_time
-    search_info['Total runtime in seconds: '] = total_time
+    search_info[step_01_label] = step_01_time
+    search_info[step_02_label] = step_02_time
+    search_info[step_03_label] = step_03_time
+    search_info[step_04_label] = step_04_time
+    search_info[total_label]   = total_time
 
     return search_info, search_result
